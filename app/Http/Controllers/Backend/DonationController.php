44a4f74df9523b0f328956d421cd58291a8b8572
DanJFletcher <dannyjfletcher@outlook.com>
2017-08-03 12:18:31 -0400

    Psr2 in donation controller

--- app/Http/Controllers/Backend/DonationController.php
+++ app/Http/Controllers/Backend/DonationController.php
@@ -12,22 +12,21 @@ use App\Models\Dashboard\Event;
 use App\Notifications\DonationMade;
 use Auth;
 
-
-
 class DonationController extends Controller
 {
     public function index()
     {
         $id = Auth::user()->id;
-        if(!empty(Auth::user()->currentTeam->id))
-        {
-          $team_id = Auth::user()->currentTeam->id;
-          $team_donations = Customer::orderBy('id','desc')->where('team_id', $team_id)->Paginate(5,['*'], 'team_donations'  );
-          $customers = Customer::orderBy('id','desc')->where('user_id', $id)->Paginate(5,['*'],'customers');
+        if (!empty(Auth::user()->currentTeam->id)) {
+            $team_id = Auth::user()->currentTeam->id;
+            $team_donations = Customer::orderBy('id', 'desc')
+                ->where('team_id', $team_id)
+                ->Paginate(5, ['*'], 'team_donations');
+            $customers = Customer::orderBy('id', 'desc')->where('user_id', $id)->Paginate(5, ['*'], 'customers');
 
-         return view('backend.donations.index', compact('customers','team_donations'));
+            return view('backend.donations.index', compact('customers', 'team_donations'));
         }
-        $customers = Customer::orderBy('id','desc')->where('user_id', $id)->take(5)->get();
+        $customers = Customer::orderBy('id', 'desc')->where('user_id', $id)->take(5)->get();
         return view('backend.donations.index', compact('customers'));
     }
 
@@ -37,88 +36,86 @@ class DonationController extends Controller
 
         $stripe = Stripe::make(env('STRIPE_SECRET'), '2016-07-06');
         try {
-                $token = $stripe->tokens()->create([
-                    'card' => [
-                        'number'    => $request->get('cardNumber'),
-                        'exp_month' => $request->get('cardExpiryMonth'),
-                        'exp_year'  => $request->get('cardExpiryYear'),
-                        'cvc'       => $request->get('cardCVC'),
-                        'name'      => $request->get('name')
+            $token = $stripe->tokens()->create([
+                'card' => [
+                    'number'    => $request->get('cardNumber'),
+                    'exp_month' => $request->get('cardExpiryMonth'),
+                    'exp_year'  => $request->get('cardExpiryYear'),
+                    'cvc'       => $request->get('cardCVC'),
+                    'name'      => $request->get('name')
 
-                    ],
-                ]);
+                ],
+            ]);
 
-                if (!isset($token['id'])) {
-                    \Session::put('error','The Stripe Token was not generated correctly');
-                    return redirect()->back();
-                }
-                $charge = $stripe->charges()->create([
-                    'card' => $token['id'],
-                    'currency' => 'CAD',
-                    'amount'   => $request->input('amount'),
-                    'receipt_email' => $request->input('email'),
-                ]);
-
-                if($charge['status'] == 'succeeded') {
-
-                    $customer = new $customers;
-                    $customer->team_id = $request->team_id;
-                    $customer->user_id = $request->user_id;
-                    $customer->event_id = 5;
-                    $customer->name = $request->name;
-                    $customer->email = $request->email;
-                    $customer->address = $request->address;
-                    $customer->city = $request->city;
-                    $customer->postal = $request->postal;
-                    $customer->comment = $request->comment;
-                    $customer->stripe_id = $charge['id'];
-                    $customer->balance_transaction = $charge['balance_transaction'];
-                    $customer->amount = $request->amount;
-                    $customer->last_four = $charge['source']['last4'];
-                    $customer->save();
-                    /**
-                    * Send Email to Customer and Admin using mailable.
-                    */
-
-                    /**
-                    * Send Email to Admin using notification.
-                    */
-
-
-                    if($customer)
-                    {
-                      $event_total = Customer::orderBy('id','desc')->where('team_id', $customer->team_id)->get();
-
-                      \DB::table('totals')
-                      ->where('event_id',  $customer->event_id)
-                      ->update(['total_donations' => $event_total->sum('amount')]);
-
-                      \DB::table('team_totals')
-                      ->where('team_id',  $customer->team_id)
-                      ->update(['total_donations' => $event_total->sum('amount')]);
-                    }
-
-
-                    \Session::put('success','Donation was successful');
-                    return redirect()->back();
-                } else {
-                    \Session::put('error','Donation was unsuccessful!!');
-                    return redirect()->back();
-                }
-            /**
-            * Start to Catch Errors
-            */
-             } catch (Exception $e) {
-                \Session::put('error',$e->getMessage());
-                return redirect()->back();
-            } catch(\Cartalyst\Stripe\Exception\CardErrorException $e) {
-                \Session::put('error',$e->getMessage());
-                return redirect()->back();
-            } catch(\Cartalyst\Stripe\Exception\MissingParameterException $e) {
-                \Session::put('error',$e->getMessage());
+            if (!isset($token['id'])) {
+                \Session::put('error', 'The Stripe Token was not generated correctly');
                 return redirect()->back();
             }
 
+            $charge = $stripe->charges()->create([
+                'card' => $token['id'],
+                'currency' => 'CAD',
+                'amount'   => $request->input('amount'),
+                'receipt_email' => $request->input('email'),
+            ]);
+
+            if ($charge['status'] == 'succeeded') {
+                $customer = new $customers;
+                $customer->team_id = $request->team_id;
+                $customer->user_id = $request->user_id;
+                $customer->event_id = 5;
+                $customer->name = $request->name;
+                $customer->email = $request->email;
+                $customer->address = $request->address;
+                $customer->city = $request->city;
+                $customer->postal = $request->postal;
+                $customer->comment = $request->comment;
+                $customer->stripe_id = $charge['id'];
+                $customer->balance_transaction = $charge['balance_transaction'];
+                $customer->amount = $request->amount;
+                $customer->last_four = $charge['source']['last4'];
+                $customer->save();
+                /**
+                * Send Email to Customer and Admin using mailable.
+                */
+
+                /**
+                * Send Email to Admin using notification.
+                */
+
+
+                if ($customer) {
+                    $event_total = Customer::orderBy('id', 'desc')->where('team_id', $customer->team_id)->get();
+
+                    \DB::table('totals')
+                    ->where('event_id', $customer->event_id)
+                    ->update(['total_donations' => $event_total->sum('amount')]);
+
+                    \DB::table('team_totals')
+                    ->where('team_id', $customer->team_id)
+                    ->update(['total_donations' => $event_total->sum('amount')]);
+                }
+
+
+                \Session::put('success', 'Donation was successful');
+                return redirect()->back();
+            } else {
+                \Session::put('error', 'Donation was unsuccessful!!');
+                return redirect()->back();
+            }
+        /**
+        * Start to Catch Errors
+        */
+        } catch (Exception $e) {
+            \Session::put('error', $e->getMessage());
+            return redirect()->back();
+        } catch (\Cartalyst\Stripe\Exception\CardErrorException $e) {
+            \Session::put('error', $e->getMessage());
+            return redirect()->back();
+        } catch (\Cartalyst\Stripe\Exception\MissingParameterException $e) {
+            \Session::put('error', $e->getMessage());
+            return redirect()->back();
+        }
     }
 }
 
