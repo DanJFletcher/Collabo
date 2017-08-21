9832cec55d6efc5fbb2cad7c618df4079216aabc
DanJFletcher <dannyjfletcher@outlook.com>
2017-08-21 18:46:19 -0400

    Set default for total donations field

--- database/migrations/2017_05_09_201450_create_team_totals_table.php
+++ database/migrations/2017_05_09_201450_create_team_totals_table.php
@@ -17,7 +17,7 @@ class CreateTeamTotalsTable extends Migration
             $table->increments('id');
             $table->unsignedInteger('team_id');
             $table->unsignedInteger('event_id')->nullable();
-            $table->float('total_donations',10,2);
+            $table->float('total_donations',10,2)->default(0);
             $table->timestamps();
         });
     }
