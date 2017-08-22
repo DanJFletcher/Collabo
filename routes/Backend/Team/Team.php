c2c2c9d887ee516b0be98550cf7f68645ed246dd
DanJFletcher <dannyjfletcher@outlook.com>
2017-08-22 19:36:49 -0400

    Rename routes and namespace to match restructure

--- routes/Backend/Team/Team.php
+++ routes/Backend/Team/Team.php
@@ -1,26 +1,34 @@
 <?php
 
-    Route::get('/', 'TeamController@index')->name('teams.index');
-    Route::get('create', 'TeamController@create')->name('teams.create');
-    Route::post('teams', 'TeamController@store')->name('teams.store');
-    Route::get('edit/{id}', 'TeamController@edit')->name('teams.edit');
-    Route::put('edit/{id}', 'TeamController@update')->name('teams.update');
-    Route::delete('destroy/{id}', 'TeamController@destroy')->name('teams.destroy');
+Route::group([
+    'prefix'     => 'teams',
+    'as'         => 'teams.',
+    'namespace' => 'Teamwork'
 
-    Route::get('switch/{id}', 'TeamController@switchTeam')->name('teams.switch');
-    Route::post('join', 'TeamController@joinTeam')->name('teams.join');
-    Route::get('view-join/{id}', 'TeamController@seeTeam')->name('teams.view');
+], function () {
 
-    Route::get('members/{id}', 'TeamMemberController@show')->name('teams.members.show');
-    Route::get('members/resend/{invite_id}', 'TeamMemberController@resendInvite')->name('teams.members.resend_invite');
-    Route::post('members/{id}', 'TeamMemberController@invite')->name('teams.members.invite');
-    Route::delete('members/{id}/{user_id}', 'TeamMemberController@destroy')->name('teams.members.destroy');
+    Route::get('/', 'TeamController@index')->name('index');
+    Route::get('create', 'TeamController@create')->name('create');
+    Route::post('teams', 'TeamController@store')->name('store');
+    Route::get('edit/{id}', 'TeamController@edit')->name('edit');
+    Route::put('edit/{id}', 'TeamController@update')->name('update');
+    Route::delete('destroy/{id}', 'TeamController@destroy')->name('destroy');
 
-    Route::delete('members/leave/{id}/{user_id}', 'TeamMemberController@leave')->name('teams.members.leave');
+    Route::get('switch/{id}', 'TeamController@switchTeam')->name('switch');
+    Route::post('join', 'TeamController@joinTeam')->name('join');
+    Route::get('view-join/{id}', 'TeamController@seeTeam')->name('view');
 
-    Route::get('accept/{token}', 'AuthController@acceptInvite')->name('teams.accept_invite');
-    Route::get('deny/{token}', 'AuthController@denyInvite')->name('teams.deny_invite');
+    Route::get('members/{id}', 'TeamMemberController@show')->name('members.show');
+    Route::get('members/resend/{invite_id}', 'TeamMemberController@resendInvite')->name('members.resend_invite');
+    Route::post('members/{id}', 'TeamMemberController@invite')->name('members.invite');
+    Route::delete('members/{id}/{user_id}', 'TeamMemberController@destroy')->name('members.destroy');
 
-    Route::get('create/event/{id}', 'TeamController@event')->name('teams.create.event');
+    Route::delete('members/leave/{id}/{user_id}', 'TeamMemberController@leave')->name('members.leave');
+
+    Route::get('accept/{token}', 'AuthController@acceptInvite')->name('accept_invite');
+    Route::get('deny/{token}', 'AuthController@denyInvite')->name('deny_invite');
+
+    Route::get('create/event/{id}', 'TeamController@event')->name('create.event');
+});
 //
 //->middleware('teamowner')
